7bf37bb70dfc7ef4ac0d001f65d63205f763ea47
momen ashraf <momwna13@gmail.com>
2025-03-28 07:27:42 +0200

    Ref Input

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,17 +1,18 @@
-import { InputHTMLAttributes, } from "react";
+import { forwardRef, InputHTMLAttributes, Ref, } from "react";
 
 interface IProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 import React from 'react'
 
-const Input = ({...rest}:IProps) => {
+const Input =forwardRef( ({...rest}:IProps,ref:Ref<HTMLInputElement> ) => {
   return (
     <input
+    ref={ref}
       className="border-[1px] border-gray-300 shadow-lg focus:border-indigo-600 focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-lg px-3 py-3 text-md w-full bg-transparent"
       {...rest}
     />
   );
 }
-
+)
 export default Input
 
