d234d8e84e2d76b7477cb6a60a43c94b61d6d390
momen ashraf <momwna13@gmail.com>
2025-03-28 07:55:41 +0200

    rouls valtiont and print error

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,7 @@
 import Button from "../components/ui/Button";
 import Input from "../components/ui/Input";
 import { useForm, SubmitHandler } from "react-hook-form"
+import InputErrorMessage from "../components/ui/InputErrorMessage";
 
 interface IFormInput {
   username: string,
@@ -22,10 +23,29 @@ const RegisterPage = () => {
         Register to get access!
       </h2>
       <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
-        <Input placeholder="username"  {...register("username",{required:"username is requird"})} />
-        <p>{errors?.username?.message}</p>
-        <Input placeholder="email"  {...register("email",{required:"email is requird"})}/> 
-        <Input placeholder="password"  {...register("password",{required:"password is requird"})}/> 
+        <div>
+        <Input placeholder="username"  {...register("username",{required:"username is requird",minLength:5})} />
+       {errors?.username && errors.username.type==="required" && <InputErrorMessage msg="User is required" /> }
+       {errors?.username && errors.username.type==="minLength" && <InputErrorMessage msg="Not valid username"/>}
+       
+          </div>
+      
+       <div>
+       <Input placeholder="email"  {...register("email",{required:"email is requird",pattern:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/})}/> 
+        {errors?.email && errors.email.type==="required" && <InputErrorMessage msg="email is require"/>}
+       {errors?.email && errors.email.type==="pattern" && <InputErrorMessage msg="Not valid email"/>}
+
+        
+        
+       </div>
+       <div>
+       <Input placeholder="password"  {...register("password",{required:"password is requird",minLength:6})}/> 
+        {errors?.password && errors.password.type==="required" && <InputErrorMessage msg="password is require"/>}
+       {errors?.password && errors.password.type==="minLength" && <InputErrorMessage msg="Not valid password "/>}
+
+       </div>
+       
+     
 
       
         <Button fullWidth >
