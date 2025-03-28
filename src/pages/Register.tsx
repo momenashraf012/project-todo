8cd943f93ffbc0db8782bbc10f742457f97ebaaf
momen ashraf <momwna13@gmail.com>
2025-03-28 05:43:03 +0200

    Initial commit

---
+++ src/pages/Register.tsx
@@ -0,0 +1,24 @@
+import Button from "../components/ui/Button";
+
+const RegisterPage = () => {
+ 
+
+  // Renders
+
+  return (
+    <div className="max-w-md mx-auto">
+      <h2 className="mb-4 text-3xl font-semibold text-center">
+        Register to get access!
+      </h2>
+      <form>
+      
+        <Button fullWidth >
+          Register jjj
+          
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterPage;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <section className="max-w-2xl mx-auto">
+    kjjk
+    </section>
+  );
+};
+
+export default HomePage;
